7e0c9cb670d9b6ac51f2da08089fd2a9d5c850e8
Jade Kim <jade@Jade-Air.local>
2022-07-18 19:26:44 +0900

    antd 기본적용

--- src/product/index.js
+++ src/product/index.js
@@ -1,8 +1,45 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./index.css";
 
 function ProductPage() {
   const { id } = useParams();
-  return <h1>상품 상세 페이지 {id} 상품</h1>;
+  const [product, setProduct] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        `https://c0c43cb4-3fab-45ce-893e-ee2a05ff46fc.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}원</div>
+        <div id="createdAt">2022년 7월 16일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
